02105cd07895fb0f212f2880a3931d13b672668a
Manthan Sharma <manthansharma79826@gmail.com>
2022-10-17 20:13:03 +0530

    moved lib to old lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'package:ekaksha/Pages/forgot.dart';
-import 'package:ekaksha/Pages/main_screen.dart';
-
-import 'Pages/login.dart';
-import 'Pages/register.dart';
 import 'package:flutter/material.dart';
-import 'Constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,26 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eKaksha',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          appBarTheme: AppBarTheme(
-              backgroundColor: Colors.indigo[900],
-              foregroundColor: Colors.white),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.indigo[900]),
-                fixedSize: MaterialStatePropertyAll(Size(90, 40))),
-          )),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Login(),
-        '/login': (context) => Login(),
-        '/register': (context) => Register(),
-        '/home': (context) => MainScreen(),
-        '/forget': (context) => Forgot(),
-      },
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('eKaksha')),
+      ),
     );
   }
 }
---
+++ oldlib/main.dart
@@ -0,0 +1,41 @@
+import 'Pages/forgot.dart';
+import 'Pages/main_screen.dart';
+
+import 'Pages/login.dart';
+import 'Pages/register.dart';
+import 'package:flutter/material.dart';
+import 'Constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'eKaksha',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.indigo[900],
+              foregroundColor: Colors.white),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.indigo[900]),
+                fixedSize: MaterialStatePropertyAll(Size(90, 40))),
+          )),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Login(),
+        '/login': (context) => Login(),
+        '/register': (context) => Register(),
+        '/home': (context) => MainScreen(),
+        '/forget': (context) => Forgot(),
+      },
+    );
+  }
+}
