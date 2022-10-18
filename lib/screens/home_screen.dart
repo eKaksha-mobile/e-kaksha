76a3580c3627461ab4f398a5f388d75462e89289
Manthan Sharma <manthansharma79826@gmail.com>
2022-10-18 10:25:33 +0530

    gradle and homescreen update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,20 +13,32 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
       body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text("Signed Out Successfully"),
-                ),
-              );
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
-            });
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/construction.png'),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text("Logout"),
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((value) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Signed Out Successfully"),
+                    ),
+                  );
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignInScreen()));
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
