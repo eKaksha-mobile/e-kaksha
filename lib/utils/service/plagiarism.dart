412044125acb9c4cb29ac4043cf6b686ac3af3e5
Manthan Sharma <manthansharma79826@gmail.com>
2022-12-01 22:51:31 +0530

    created plagarism checkfunctionality

--- lib/utils/service/plagiarism.dart
+++ lib/utils/service/plagiarism.dart
@@ -1,70 +1,38 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
-// import 'networking.dart';
-
-const apiKey = '64d4144c-d85e-4c0e-962c-3d3ddc49bad7';
-const copyLeaksURL = 'https://id.copyleaks.com/v3/account/login/api';
+// 'X-RapidAPI-Key': '568ce5fb4amsh22f37fe760d2699p102d6ejsnbb531fab6a56',
+// 'X-RapidAPI-Host': 'plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com'
 
 class Plagiarism {
-  Future<dynamic> getScore(String cityName) async {
-    var data = {
-      "url": "https://example.com",
-      "properties": {
-        "sandbox": true,
-        "webhooks": {"status": "https://yoursite.com/webhook/{STATUS}"}
-      }
-    };
-
-    // NetworkHelper networkHelper =
-    //     NetworkHelper('$copyLeaksURL?q=$cityName&appid=$apiKey&units=metric');
-
-    // var weatherData = await networkHelper.getData();
-    // return weatherData;
-
-    var url = '{{baseURL}}/api/auth/login';
-    // var body = json.encode({
-    //   'nick': nick,
-    //   'password': password,
-    // });
-
-    // print('Body: $body');
-
+  Future getData(String text) async {
     var response = await http.post(
-      Uri.parse(data['url'] as String),
-      headers: {
-        'accept': 'application/json',
-        'Content-Type': 'application/json-patch+json',
-      },
-    );
+        Uri.parse(
+            'https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism'),
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key':
+              '568ce5fb4amsh22f37fe760d2699p102d6ejsnbb531fab6a56',
+          'X-RapidAPI-Host':
+              'plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com'
+        },
+        body: jsonEncode({
+          "text": text,
+          "language": "en",
+          "includeCitations": false,
+          "scrapeSources": false
+        }));
 
-    // todo - handle non-200 status code, etc
+    if (response.statusCode == 200) {
+      String data = response.body;
 
-    // return json.decode(response.body);
-  }
-
-  Future<Response> loginAndGetAccessToken() async {
-    var response = await http.post(
-      Uri.parse('https://id.copyleaks.com/v3/account/login/api'),
-      body: {
-        "email": "ekaksha.official.app@gmail.com",
-        "key": "64d4144c-d85e-4c0e-962c-3d3ddc49bad7"
-      },
-    );
-    return response;
-  }
-
-  void submitUrlScan(String scanId) async {
-    var response = await http.post(
-      Uri.parse('https://api.copyleaks.com/v3/scans/submit/url/$scanId'),
-      body: {
-        "url": "https://example.com",
-        "properties": {
-          "sandbox": true,
-          "webhooks": {"status": "https://yoursite.com/webhook/{STATUS}"}
-        }
-      },
-    );
+      // print(jsonDecode(data));
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
     // return response;
   }
 }
